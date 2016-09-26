3f98b797fe4452fd15cd2af5a71ba094c0c7a5db
jca348 <jca348@asb9840u-d03.csil.sfu.ca>
2016-09-26 15:11:53 -0700

    rename debug.h

---
+++ common.h
@@ -0,0 +1,12 @@
+/* The common settings for the whole project, this header should 
+be included in other headers in this project.*/
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#define DEBUG 
+
+
+#endif
