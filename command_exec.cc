860f95683a2da3aca9184daace200eff1bd5a060
CJC <cjc0722hz@gmail.com>
2016-10-01 20:56:32 -0700

    add command execution

---
+++ command_exec.cc
@@ -0,0 +1,26 @@
+#include "command_exec.h"
+
+// void exec_command(Command c)
+// {
+// 	c.check_type();
+// 	switch(c.type){
+// 		case EXIT :
+// 			exit(0);
+// 			break;
+// 		case CD :
+
+// 			break;
+// 		case JOBS :
+// 			break;
+// 		case FG :
+// 			break;
+// 		case BG :
+// 			break;
+// 		default :
+			
+
+
+
+// 	}
+
+// }
---
+++ command_exec.h
@@ -0,0 +1,15 @@
+#ifndef COMMAND_EXEC_H
+#define COMMAND_EXEC_H
+#include "common.h"
+#include "structure.h"
+
+// void exec_command(Command c); // return type not determined yet
+
+
+
+
+
+
+
+
+#endif
