c1555ec8d01a5301e1a70cc8841381849e9af7d2
Jack Liang <bunnytooth99@gmail.com>
2019-03-22 15:41:05 -0700

    Revised grammar
    'the type of commands'  -> 'the type of command'

--- command_exec.h
+++ command_exec.h
@@ -6,7 +6,7 @@
 #include "basic.h"
 #include "job_control.h"
 
-/* This is the high-level function which decides what to do according to the type of commands. */
+/* This is the high-level function which decides what to do according to the type of command. */
 void exec_command		(Job *job); // return type not determined yet
 
 /* Execute the specified job, including launch each process in the job and set up the pipes and
